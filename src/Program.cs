5be6f9c7426d4ba87725d4805241bcd477ac494a
Guilherme Fernandes <gui.fer.sv@gmail.com>
2025-09-18 17:24:23 -0300

    Serilog seting

--- src/Program.cs
+++ src/Program.cs
@@ -1,18 +1,38 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-  app.MapOpenApi();
-  app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "v1"));
+  var builder = WebApplication.CreateBuilder(args);
+
+  builder.Services.AddSerilog((services, ls) => ls
+      .ReadFrom.Configuration(builder.Configuration)
+      .ReadFrom.Services(services)
+      .Enrich.FromLogContext()
+      .WriteTo.Console());
+  builder.Services.AddOpenApi();
+
+  var app = builder.Build();
+
+  // Configure the HTTP request pipeline.
+  if (app.Environment.IsDevelopment())
+  {
+    app.MapOpenApi();
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "v1"));
+  }
+
+  app.UseHttpsRedirection();
+
+  app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.Run();
+catch (Exception ex)
+{
+  Log.Fatal(ex, "Terminated unexpectedly");
+}
+finally
+{
+  Log.CloseAndFlush();
+}
