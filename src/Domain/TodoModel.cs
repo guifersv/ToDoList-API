f36c52eadc4e87808253b5965348481db711db0e
Guilherme Fernandes <gui.fer.sv@gmail.com>
2025-09-18 19:12:08 -0300

    Update model for navigation

--- src/Domain/TodoModel.cs
+++ src/Domain/TodoModel.cs
@@ -1,5 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace ToDoList.Domain;
 
 public record TodoModel(string Title, string Description, bool IsCompleted, DateTime DateCreated)
 {
+  public int Id { get; set; }
+  public int TodoListModelId { get; set; }
+  public required TodoListModel TodoListModelNavigation { get; set; }
 }
