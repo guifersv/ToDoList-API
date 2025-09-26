f2dfaa7d7af4a12356300ed8e82b24640353d47b
Guilherme Fernandes <gui.fer.sv@gmail.com>
2025-09-26 18:05:15 -0300

    Forgot Endpoint summary

--- src/Endpoints/ApiEndpoints.cs
+++ src/Endpoints/ApiEndpoints.cs
@@ -24,6 +24,7 @@ public static class ApiEndpoints
       return TypedResults.Ok(model);
   }
 
+  [EndpointSummary("Get All Todo Lists")]
   public static async Task<IEnumerable<TodoListModel>> GetAllTodoLists(ITodoService service)
   {
     return await service.GetAllTodoListsAsync();
